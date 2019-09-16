d8acdd0bfdd366f06b6d242f523e5b1991d055ae
Glendon Philipp <gpbaculio@gmail.com>
2019-09-17 02:02:08 +0800

    added change name test

--- frontend/src/__sagas_tests__/boards.test.ts
+++ frontend/src/__sagas_tests__/boards.test.ts
@@ -4,7 +4,7 @@ import * as matchers from 'redux-saga-test-plan/matchers';
 import { throwError } from 'redux-saga-test-plan/providers';
 import uuidv1 from 'uuid/v1';
 
-import Api from '../Api';
+import Api, { createBoardArgs, updateNameArgs } from '../Api';
 import boardsSagas from '../sagas/boards';
 import boardsReducer, {
   boardInitLoading,
@@ -21,9 +21,15 @@ import {
   CREATE_BOARD_REQUEST,
   BoardsState
 } from '../store/boards/types';
-import { createboardSuccess, createBoard } from '../store/boards/actions';
+import {
+  createboardSuccess,
+  createBoard,
+  updateName,
+  updateNameSuccess
+} from '../store/boards/actions';
 import { AxiosResponse } from 'axios';
 import { combineReducers } from 'redux';
+
 class BoardTypeResponse {
   created_on: string;
   name: string;
@@ -39,68 +45,141 @@ class BoardTypeResponse {
     delete_password?: string;
     _id?: string;
   }) {
-    (this.created_on = this.now()),
-      (this.name = params.name || 'TEST BOARD NAME'),
-      (this.threads = []),
-      (this.updated_on = this.now()),
-      (this._id = params._id || this.genUuidV1()),
-      (this.delete_password = params.delete_password || this.genUuidV1()),
-      (this.loading = {
-        update_name: false
-      });
+    const genId = uuidv1();
+    const genDelPass = uuidv1();
+    const dateNow = new Date().toISOString();
+    this.created_on = dateNow;
+    this.name = params.name || 'TEST BOARD NAME';
+    this.threads = [];
+    this.updated_on = dateNow;
+    this._id = params._id || genId;
+    this.delete_password = params.delete_password || genDelPass;
+    this.loading = {
+      update_name: false
+    };
     this.error = {
       update_name: ''
     };
   }
-  genUuidV1 = () => uuidv1();
-  now = () => new Date().toISOString();
 }
 describe.only('Boards Sagas', () => {
-  describe('handleFetchPostsRequest', () => {
-    it('should create board', async () => {
-      const createBoardArgs = {
-        name: 'CREATE BOARD TEST',
-        delete_password: 'abcd123'
-      };
-      const mockBoard = new BoardTypeResponse(createBoardArgs);
-      const { storeState } = await expectSaga(
-        boardsSagas.createBoard as SagaType,
-        createBoard('CREATE BOARD TEST', 'abcd123')
-      )
-        .withReducer(boardsReducer)
-        // Mock the response from API
-        .provide({
-          call: (effect, next) => {
-            // Check for the API call to return fake value
-            if (effect.fn === Api.boards.createBoard) {
-              return { data: { board: mockBoard } };
-            }
-            // Allow Redux Saga to handle other `call` effects
-            return next();
+  it('should create board', async () => {
+    const createBoardArgs: createBoardArgs = {
+      name: 'CREATE BOARD TEST',
+      delete_password: 'abcd123'
+    };
+    const mockBoard = new BoardTypeResponse(createBoardArgs);
+    const { storeState } = await expectSaga(
+      boardsSagas.createBoard as SagaType,
+      // dispatch initial action it wants to test
+      createBoard('CREATE BOARD TEST', 'abcd123')
+    )
+      .withReducer(boardsReducer)
+      // Mock the response from API
+      .provide({
+        call: (effect, next) => {
+          // Check for the API call to return fake value
+          if (effect.fn === Api.boards.createBoard) {
+            return { data: { board: mockBoard } };
           }
+          // Allow Redux Saga to handle other `call` effects
+          return next();
+        }
+      })
+      .put(
+        createboardSuccess({
+          ...mockBoard,
+          loading: boardInitLoading,
+          error: boardInitError
         })
-        // Dispatch the action that it wants to test
-        .dispatch(createBoard('CREATE BOARD TEST', 'abcd123'))
-        // Specify that it needs to call the API
-        .call(Api.boards.createBoard, createBoardArgs)
-        .put(
-          createboardSuccess({
-            ...mockBoard,
-            loading: boardInitLoading,
-            error: boardInitError
-          })
-        )
-        .run();
-      // load should be false
-      expect(storeState.loading.createBoard).toEqual(false);
-      // id of board on boards property
-      expect(storeState.boards).toHaveProperty(mockBoard._id);
-      // loading and error prop is not on axios response since we use it for UI effects
-      expect(storeState.boards[mockBoard._id]).toEqual({
-        ...mockBoard,
-        loading: boardInitLoading,
-        error: boardInitError
-      });
+      )
+      .silentRun();
+    // load should be false
+    expect(storeState.loading.createBoard).toEqual(false);
+    // id of board on boards property
+    expect(storeState.boards).toHaveProperty(mockBoard._id);
+    // loading and error prop is not on axios response since we use it for UI effects
+    expect(storeState.boards[mockBoard._id]).toEqual({
+      ...mockBoard,
+      loading: boardInitLoading,
+      error: boardInitError
     });
+  });
+  it('should update board name', async () => {
+    const createBoardArgs: createBoardArgs = {
+      name: 'CREATE BOARD TEST',
+      delete_password: 'abcd123'
+    };
+    const mockBoard = new BoardTypeResponse(createBoardArgs);
+    // create constant of args for updateName params to reference on test
+    const updateNameArgs: updateNameArgs = {
+      // use mockBoard id cause we use it for withReducer
+      board_id: mockBoard._id,
+      board_name: `UPDATED ${mockBoard.name}`,
+      // delete password must match delete password of board to succeed
+      delete_password: mockBoard.delete_password
+    };
+    const { storeState } = await expectSaga(
+      boardsSagas.updateName as SagaType,
+      // initial dispatch to test
+      updateName(updateNameArgs)
+    )
+      .withReducer(
+        combineReducers({
+          boards: boardsReducer
+        }),
+        // initial state of boards
+        {
+          boards: {
+            ...boardsInitState,
+            // assign the mock board on boards reducer
+            // with initial loading end error state
+            boards: {
+              ...boardsInitState.boards,
+              [mockBoard._id]: {
+                ...mockBoard,
+                loading: boardInitLoading,
+                error: boardInitError
+              }
+            }
+          }
+        }
+      )
+      .provide({
+        call: (effect, next) => {
+          // Check for the API call to return
+          // fake board with updated name
+          if (effect.fn === Api.boards.updateName) {
+            return {
+              data: {
+                // reference updateNameArgs
+                board: { ...mockBoard, name: updateNameArgs.board_name }
+              }
+            };
+          }
+          // Allow Redux Saga to handle other `call` effects
+          return next();
+        }
+      })
+      .put(
+        // reference updateNameArgs assuming the operation succeeds
+        updateNameSuccess({ ...mockBoard, name: updateNameArgs.board_name })
+      )
+      .run();
+
+    // load of update name on specific board should be false
+    expect(storeState.boards.boards[mockBoard._id].loading.update_name).toEqual(
+      false
+    );
+    // board on state should contain updated name
+    expect(storeState.boards.boards[mockBoard._id]).toEqual({
+      ...mockBoard,
+      name: updateNameArgs.board_name,
+      loading: boardInitLoading,
+      error: boardInitError
+    });
+  });
+  it('should add thread on board', async () => {
+    // continue here
   });
 });
